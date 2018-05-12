a82203e8273c3e7b207d17a37a93dd614f956b86
lukkre002 <lukaszcezarykreski@gmail.com>
2018-05-13 00:20:16 +0200

    [ADD] Małe poprawki wizualne

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -10,7 +10,7 @@
 </style>
 
 
-  <title>Zarejestruj się</title>
+  <title>Konferencja</title>
   <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.css')}}">
   <link rel="stylesheet" type="text/css" href="{{ url('css/jquery-ui.css')}}">
   <link rel="stylesheet" type="text/css" href="{{ url('css/style.css')}}">
